9f9a5b707d2b5cb0266c0a2582403cd78918706a
Roy Chiang <roy@Roys-MacBook-Pro.local>
2015-07-02 10:24:29 +0800

    added glob.sync so production.js can be served

--- config/init.js
+++ config/init.js
@@ -1,10 +1,18 @@
 'use strict';
 
+var glob = require('glob');
+
 module.exports = function(){
-	if (process.env.NODE_ENV) {
-		console.error('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead');
-	} else {
-		console.error('NODE_ENV is not defined! Using default development environment');
-    process.env.NODE_ENV = 'development';
-	}
+	var files = glob.sync('./config/env/' + process.env.NODE_ENV + '.js');
+		if (!files.length) {
+			if (process.env.NODE_ENV) {
+				console.error('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead');
+			} else {
+				console.error('NODE_ENV is not defined! Using default development environment');
+			}
+
+			process.env.NODE_ENV = 'development';
+		} else {
+			console.log('Application loaded using the "' + process.env.NODE_ENV + '" environment configuration');
+		}
 };
