e1965816323b64b232cee912a5a09274b45aab29
Roy Chiang <roy@Roys-MacBook-Pro.local>
2015-07-10 18:10:14 +0800

    changed api for category to // instead of http://

--- public/dist/application.js
+++ public/dist/application.js
@@ -145,7 +145,7 @@ app.controller('CategoriesEditController', ['$scope', '$stateParams', '$location
 //Categories service used to communicate Categories REST endpoints
 angular.module('core').factory('Categories', ['$resource', '$cookies',
 	function($resource, $cookies) {
-		return $resource('http://calm-woodland-4818.herokuapp.com/api/categories/:controller', '',
+		return $resource('//calm-woodland-4818.herokuapp.com/api/categories/:controller', '',
     {
 			list: {
 				method: 'GET'
--- public/modules/categories/services/categories.client.service.js
+++ public/modules/categories/services/categories.client.service.js
@@ -3,7 +3,7 @@
 //Categories service used to communicate Categories REST endpoints
 angular.module('core').factory('Categories', ['$resource', '$cookies',
 	function($resource, $cookies) {
-		return $resource('http://calm-woodland-4818.herokuapp.com/api/categories/:controller', '',
+		return $resource('//calm-woodland-4818.herokuapp.com/api/categories/:controller', '',
     {
 			list: {
 				method: 'GET'
