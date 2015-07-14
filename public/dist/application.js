351a5e836eeeb6a0a233e8e858c74dac8fcddfda
Roy Chiang <roy@Roys-MacBook-Pro.local>
2015-07-14 15:47:56 +0800

    grunt build the previous changes

--- public/dist/application.js
+++ public/dist/application.js
@@ -1040,6 +1040,20 @@ angular.module('core').factory('Products', ['$resource', '$cookies',
 
 'use strict';
 
+// Configuring the Articles module
+angular.module('core').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		// Menus.addMenuItem('topbar', 'Categories', 'categories', 'item', '/categories(?:/[^/]+)?', null, null, 9);
+    // Set top bar menu items
+		Menus.addMenuItem('topbar', 'Home', '', 'item', '/(?:/[^/]+)?', null, null, 0);
+    Menus.addMenuItem('topbar', 'About', 'about', 'item', '/about(?:/[^/]+)?', null, null, 1);
+    Menus.addMenuItem('topbar', 'Contact', 'contact', 'item', '/contact(?:/[^/]+)?', null, null, 9);
+	}
+]);
+
+'use strict';
+
 // Setting up route
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
